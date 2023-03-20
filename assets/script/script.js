930bb0c4e0113ad125b5626ad6e29b292cd305d2
Mark Daniel <markdaniel1982@gmail.com>
2023-03-20 09:20:51 +0000

    complete simple javascript so game runs and decides & displays the winner

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,46 @@
-console.log("Yo!")
+// wait for DOM to load before running the game
+
+// code credit - CI Love Maths walkthrough (dom content loaded)
+//Code Credit - 
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll("button");
+console.log("DOM Loaded")
+    for (const button of buttons) {
+        button.addEventListener("click", playGame);
+    }
+});
+
+function playGame() {
+    const playerChoice = this.id;
+    const computerChoice = computerSelect();
+
+    const result = getResult(playerChoice, computerChoice);
+    displayResult(result);
+}
+
+function computerSelect() {
+    const choices = ["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * 3)];
+}
+
+
+function getResult(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return "Draw!";
+    } else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") || 
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "Winner!";
+    } else {
+        return "You Lost!";
+    }
+}
+
+
+
+function displayResult(result){
+    const resultElement = document.getElementById("result");
+    resultElement.innerText = result;
+}
